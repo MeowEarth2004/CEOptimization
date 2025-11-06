bef42e2e938145281e881e90b47a08a073c43a67
Anaphylaxis'Heart <partyzz802@gmail.com>
2025-11-06 14:09:38 +0700

    Update 1.1.3.0

---
+++ 3_Mobile_App/EnergyApp/constants/config.js
@@ -0,0 +1,4 @@
+const API_URL = "https://unjaunty-ricarda-telephonically.ngrok-free.dev"; // แก้ไขทุกครั้งที่จะรัน EnergyApp
+// 🔴 ================== 🔴
+export const SOCKET_URL = API_URL;
+export const CONTROL_URL = `${API_URL}/control`;
--- src/config.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <WiFi.h>
-
-const char* ssid = "YOUR_WIFI";        // ใส่ชื่อ WiFi
-const char* password = "YOUR_PASS";    // ใส่รหัส WiFi
-const char* mqtt_server = "192.168.1.100";  // IP ของคอมที่รัน MQTT Broker
-const int mqtt_port = 1883;
