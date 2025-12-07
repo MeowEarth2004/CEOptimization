9ae4e25f9ba1914c3cd7f778751fe2562a2e5d17
Anaphylaxis'Heart <partyzz802@gmail.com>
2025-12-07 13:47:27 +0700

    Update 1.2.3.8

--- 3_Mobile_App/EnergyApp/constants/config.js
+++ 3_Mobile_App/EnergyApp/constants/config.js
@@ -1,6 +1,6 @@
-const API_URL = "https://unjaunty-ricarda-telephonically.ngrok-free.dev" // แก้ไขทุกครั้งที่จะรัน EnergyApp
+const API_URL = "https://unjaunty-ricarda-telephonically.ngrok-free.dev"
 //http://localhost:5500 
 // 🔴 ================== 🔴
 export const SOCKET_URL = API_URL;
 export const CONTROL_URL = `${API_URL}/control`;
-
+export const SERVER_URL = API_URL;
